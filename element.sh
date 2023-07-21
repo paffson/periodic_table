652c0e451faae8cd438e2a21d00d397121db05e8
Pawel Walczewski <pwalczewski@gmail.com>
2023-07-21 17:34:02 -0400

    feat: add initial prompt and database connection data

--- element.sh
+++ element.sh
@@ -1,1 +1,8 @@
+PSQL="psql --username=freecodecamp --dbname=periodic_table -t --no-align -c"
+
+MENU() {
 echo Please provide an element as an argument.
+}
+MENU
+
+# The element with atomic number 1 is Hydrogen (H). It's a nonmetal, with a mass of 1.008 amu. Hydrogen has a melting point of -259.1 celsius and a boiling point of -252.9 celsius.
